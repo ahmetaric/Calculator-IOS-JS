5e257b3aef850f6888a2f0a4b00bdd97ead11869
ahmetaric <ahmet.aric1965@gmail.com>
2022-06-13 23:12:02 +0300

    last version

--- app.js
+++ app.js
@@ -46,3 +46,69 @@ modul.addEventListener("click",() => {
      upscreen.innerText = downscreen.innerText;
    }
 });
+
+seven.addEventListener("click", () => {
+  upscreen.innerHTML += "7";
+});
+
+eight.addEventListener("click", () => {
+  upscreen.innerHTML += "8";
+});
+
+nine.addEventListener("click", () => {
+  upscreen.innerHTML += "9";
+});
+
+four.addEventListener("click", () => {
+  upscreen.innerHTML += "4";
+});
+
+five.addEventListener("click", () => {
+  upscreen.innerHTML += "5";
+});
+
+six.addEventListener("click", () => {
+  upscreen.innerHTML += "6";
+});
+
+one.addEventListener("click", () => {
+  upscreen.innerHTML += "1";
+});
+
+two.addEventListener("click", () => {
+  upscreen.innerHTML += "2";
+});
+
+three.addEventListener("click", () => {
+  upscreen.innerHTML += "3";
+});
+
+zero.addEventListener("click", () => {
+  upscreen.innerHTML += "0";
+});
+
+dot.addEventListener("click", () => {
+  upscreen.innerHTML += ".";
+});
+
+minus.addEventListener("click", () => {
+  upscreen.innerHTML += "-";
+});
+
+plus.addEventListener("click", () => {
+  upscreen.innerHTML += "+";
+});
+
+multiply.addEventListener("click", () => {
+  upscreen.innerHTML += "*";
+});
+
+divide.addEventListener("click", () => {
+  upscreen.innerHTML += "/";
+});
+
+equal.addEventListener("click", () => {
+  let final = eval(upscreen.innerText);
+  downscreen.innerText = final.toFixed(2);
+  upscreen.innerText = downscreen.innerText;
+});
